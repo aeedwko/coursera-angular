301537220548a6bc93e9d9481722ba6870f5de6e
Edwin <edwin.mk.ko@gmail.com>
2017-01-10 01:41:14 -0800

    module 1

---
+++ module1_solution/scripts/app.js
@@ -0,0 +1,32 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchController', LunchController);
+
+  LunchController.$inject = ['$scope'];
+  function LunchController($scope) {
+    $scope.input = "";
+    $scope.count = 0;
+    $scope.message= "";
+    $scope.updateMessage = function() {
+      if ($scope.input == "") {
+        $scope.message = "Please enter data first";
+      } else if ($scope.count <= 3) {
+        $scope.message = "Enjoy!";
+      } else if ($scope.count > 3) {
+        $scope.message = "Too much!";
+      }
+    };
+
+    $scope.countDishes = function() {
+      var array = $scope.input.split(",");
+      $scope.count = array.length;
+      for (var i=0; i < array.length; i++) {
+        if (array[i].trim() == "") {
+          $scope.count -= 1;
+        }
+      }
+    }
+  }
+})();
